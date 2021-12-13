255977f8c3267404bb21cc69000b6a22d1bb5fa1
19pa1a0439 <60639448+19pa1a0439@users.noreply.github.com>
2021-12-13 22:43:36 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,16 @@
+public class Player{
+	private int playerName;
+	private int playerLocation;
+	Player(int playerName){
+		this.playerName=playerName;
+	}
+	public String getplayerName(){
+		return(playerName);
+	}
+	public void setPlayerLocation(int playerLocation){
+		this.playerLocation=playerLocation;
+	}
+	public int getPlayerLocation(){
+		return(playerLocation);
+	}
+		
